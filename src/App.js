1e47472556952370c61f54728d5b691ce6f147cd
BrunoApSouza <brunoap500@gmail.com>
2024-05-28 21:37:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={mba} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Welcome to UFSCAR</h1>
+          <h1 className="App-title"> Welcome to MBA Impacta</h1>
         </header>
       </div>
     );
