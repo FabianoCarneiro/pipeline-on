6ab152fb828dfeaec8e50fda5bcc5a3d0dc33e0c
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2024-05-28 21:52:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import mba from './3.webp';
 import './App.css';
 
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={mba} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
 
           <p className="App-intro">
             Teste Fernando
