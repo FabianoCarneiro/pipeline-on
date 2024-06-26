09df4f83437c75ae3969a2d3ae7327e1b0fc887c
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2024-06-25 21:25:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
           <h1 className="App-title"> Welcome to MBA Impacta</h1>
-
         </header>
       </div>
     );
