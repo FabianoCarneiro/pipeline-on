827e71c7a1ab11ce1b9c3b8bc69de97374d17107
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2025-09-15 21:14:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
           <img src={logo} className="App-logo" alt="logo" />
 
-          <h1 className="App-title"> Essa é Feature AS10/h1>
+          <h1 className="App-title"> Essa é Feature AS10</h1>
 
       </header>
 
