2db6f75fa87a33251e7ef011cb16831126f97779
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2025-09-15 21:12:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,10 +14,7 @@ class App extends Component {
 
           <img src={logo} className="App-logo" alt="logo" />
 
-          <h1 className="App-title"> Essa é Feature 5AGLR 1.0.1</h1>
-
-          <h1 className="App-title">Essa é Feature 5AGLR 2</h1>
-
+          <h1 className="App-title"> Essa é Feature AS10/h1>
 
       </header>
 
