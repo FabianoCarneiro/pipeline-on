732b3056d5466d0f0c94e7e13b7f2953ceb4594e
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2025-10-08 21:49:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
           <img src={logo} className="App-logo" alt="logo" />
 
-          <h1 className="App-title"> Essa é Feature DVPR 07 2025</h1>
+          <h1 className="App-title"> Essa é Feature DVPR 07 01</h1>
 
       </header>
 
