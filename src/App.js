7720ea3b397a87da75fb76760c1f69eaa643383c
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2026-01-28 21:48:54 -0300

    Update App title to include year 2026

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
           <img src={logo} className="App-logo" alt="logo" />
 
-          <h1 className="App-title">Essa é Feature 11CLC F2</h1>
+          <h1 className="App-title">Essa é Feature 11CLC F2 2026</h1>
 
       </header>
 
