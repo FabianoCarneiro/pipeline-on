4919da400cecf8354312cf8a1518287773cb6edb
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2026-03-26 21:14:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
 
 
-          <h1 className="App-title">Essa é Feature AS11 2026</h1>
+          <h1 className="App-title">Essa é Feature AS11 01 2026</h1>
 
         </header>
       </div>
