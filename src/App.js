3deab7bce3e501de5dd26b56db7ad68e33c9bb4b
FabianoCarneiro <37809484+FabianoCarneiro@users.noreply.github.com>
2026-03-30 21:13:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
           <img src={logo} className="App-logo" alt="logo" />
 
-          <h1 className="App-title">Essa é Feature CTO01 01</h1>
+          <h1 className="App-title">Essa é Feature CTO02 2026 01</h1>
 
         </header>
       </div>
